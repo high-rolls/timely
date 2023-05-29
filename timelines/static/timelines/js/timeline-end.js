b5f9f55fe5297d9c11a7d8dab3262d94d5486bbb
Paulo Vitor Ferreira Goncalves Pivotto <paulo_vitor652@hotmail.com>
2023-05-29 20:36:19 -0300

    automatic setting end_date.value

--- timelines/static/timelines/js/timeline-end.js
+++ timelines/static/timelines/js/timeline-end.js
@@ -12,3 +12,23 @@ addEventButton.addEventListener("click", function() {
     end_date.value = formattedEndDate;
     console.log(formattedStartDate);
 });
+
+var start_date = document.getElementById("start_date");
+start_date.addEventListener("input", function() {
+    var end_date = document.getElementById("end_date");
+    var date = new Date(start_date.value);
+    date.setDate(date.getDate() + 1);
+    var formattedEndDate = date.toISOString().split("T")[0];
+    end_date.value = formattedEndDate;
+});
+
+var end_date = document.getElementById("end_date");
+end_date.addEventListener("change", function() {
+    var start_date = document.getElementById("start_date");
+    var startDate = new Date(start_date.value);
+    var endDate = new Date(end_date.value);
+    if (endDate < startDate) {
+        alert("End date must be after start date.");
+        end_date.value = start_date.value;
+    };
+});
